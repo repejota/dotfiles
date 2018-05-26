d7828ceaa95ca5f1958632fbb2d4c8939798e9a4
Raül Pérez <repejota@gmail.com>
2018-05-26 21:33:41 +0200

    fallback to ls if exa is not available

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -2,9 +2,11 @@
 colorflag="-G"
 alias ls="ls -la ${colorflag}"
 
-# Instead of ls i use exa
-alias ls="/usr/local/bin/exa"
-alias l="/usr/local/bin/exa -la --git"
+# Instead of ls use exa
+if [ -x /usr/local/bin/exa ]; then
+    alias ls="/usr/local/bin/exa"
+    alias l="/usr/local/bin/exa -la --git"
+fi
 
 # SSH Agent
 alias ssh="ssh -A"
