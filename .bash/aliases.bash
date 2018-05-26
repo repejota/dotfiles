2143a279e6af70273d694c7ed567d20b192c2be1
Raül Pérez <repejota@gmail.com>
2018-05-26 22:34:21 +0200

    negative if

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -3,7 +3,7 @@ alias ls="ls -lA"
 alias l=ls
 
 # Instead of ls use exa
-if [ -x $(which exa) ]; then
+if [ $(which exa) ]; then
     alias ls="/usr/local/bin/exa"
     alias l="/usr/local/bin/exa -la --git"
 fi
@@ -12,9 +12,9 @@ fi
 alias ssh="ssh -A"
 
 # Docker
-if [ -x $(which docker-compose) ]; then
+if [ $(which docker-compose) ]; then
     alias dc="docker-compose"
 fi
-if [ -x $(which docker-machine) ]; then
+if [ $(which docker-machine) ]; then
     alias dm="docker-machine"
 fi
